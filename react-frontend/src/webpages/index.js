1c40a6a990e4fadb9f231e89ea09d10661cdd284
Aiswarya Sudhakaran <asudhakaran@enableconsulting.com>
2023-10-07 17:17:02 +0530

    Restaurant creation completed

--- react-frontend/src/webpages/index.js
+++ react-frontend/src/webpages/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './home';
-import Login from './login';
-import CreateRestaurant from './create_restaurant';
+import Home from "./home";
+import Login from "./login";
+import CreateRestaurant from "./create_restaurant";
+import CreateAdmin from "./create_admin";
 
 const Webpages = () => {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/:restaurantId/login" element={<Login />} />
         <Route path="/create" element={<CreateRestaurant />} />
+        <Route path="/admin/create" element={<CreateAdmin />} />
       </Routes>
-    </BrowserRouter>)
-}
+    </BrowserRouter>
+  );
+};
 
 export default Webpages;
