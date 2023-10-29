fe9d6c86301fd667fb63c0840a19b151be63a5d7
Aiswarya Sudhakaran <2022mt93702@wilp.bits-pilani.ac.in>
2023-10-29 09:41:57 +0530

    Order creation completed

--- react-frontend/src/webpages/components/orders/order_form.js
+++ react-frontend/src/webpages/components/orders/order_form.js
@@ -6,19 +6,31 @@ import {
   GET_MENUS,
 } from "../../apis/api";
 
-import { GetIdFromUrl, getLocalStorageValue } from "../../helper";
+import {
+  GetIdFromUrl,
+  capitalizeFirstLetters,
+  getLocalStorageValue,
+} from "../../helper";
 import { useNavigate, useParams } from "react-router-dom";
 
 const OrderForm = ({ edit }) => {
   const { orderId } = useParams();
   const restaurantId = useRef(GetIdFromUrl("restaurant_id"));
+  const userId = getLocalStorageValue("user_id");
   const navigate = useNavigate();
   const submitHttpCode = edit ? "PATCH" : "POST";
   const submitApi = edit ? GET_SINGLE_ORDER(orderId) : GET_ORDERS;
   const [error, setError] = useState("");
-  const [formData, setFormData] = useState();
-  const [tableOption, setTableOption] = useState([]);
+  const [formData, setFormData] = useState({
+    tables: [],
+    menus: [],
+    staff_id: userId,
+  });
+
+  const [tableData, setTableData] = useState([]);
   const [menuData, setMenuData] = useState([]);
+
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
@@ -54,7 +66,7 @@ const OrderForm = ({ edit }) => {
           }
         );
         const data = await response.json();
-        setTableOption(data);
+        setTableData(data);
       } catch (error) {
         setError(error);
       }
@@ -105,22 +117,122 @@ const OrderForm = ({ edit }) => {
     }
   };
 
-  const handleChange = (e) => {
-    e.preventDefault();
+  const calculateTotal = (updatedMenus) => {
+    let total = 0;
+    updatedMenus.forEach((menu) => {
+      const menuItem = menuData.find(
+        (menuItem) => menuItem.id === menu.menu_id
+      );
+      if (menuItem) {
+        total += menu.quantity * menuItem.price;
+      }
+    });
+    return total;
+  };
+
+  const handleTableSelection = (e) => {
+    const value = e.target.value;
+    const updatedTables = [...formData.tables]; // Create a copy of the tables array
+    const index = updatedTables.findIndex((table) => table.table_id === value);
+    if (index !== -1) {
+      // If the table is already selected, remove it
+      updatedTables.splice(index, 1);
+    } else {
+      // If it's not selected, add it
+      updatedTables.push({ table_id: value });
+    }
+    setFormData({ ...formData, tables: updatedTables });
+  };
+
+  const handleMenuSelection = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    const intValue = Number(value);
+    const updatedMenus = [...formData.menus]; // Create a copy of the tables array
+    const index = updatedMenus.findIndex((menu) => menu.menu_id === name);
+
+    if (index !== -1) {
+      if (value <= 0) updatedMenus.splice(index, 1);
+      // If the menu is already selected, remove it
+      else updatedMenus[index].quantity = intValue;
+    } else {
+      if (value <= 0) return;
+      // If it's not selected, add it
+      else updatedMenus.push({ menu_id: name, quantity: intValue });
+    }
+    setFormData({ ...formData, menus: updatedMenus });
+    setTotal(calculateTotal(updatedMenus));
   };
 
   return (
     <div>
       {error && <div className="error">{error}</div>}
       <form onSubmit={handleSubmit}>
+        <div className="title">
+          <label>Tables:</label>
+        </div>
+        <div className="tableSection">
+          <div className="checkboxes">
+            {tableData.map((table) => (
+              <label key={table.id}>
+                <input
+                  type="checkbox"
+                  id={table.id}
+                  name="tableSelected"
+                  value={table.id}
+                  onChange={handleTableSelection}
+                  checked={formData.tables.some(
+                    (tableData) => tableData.table_id === table.id
+                  )}
+                />
+                {table.table_number}
+              </label>
+            ))}
+          </div>
+        </div>
+        <div className="title">
+          <label>Menus:</label>
+        </div>
+        <div className="menuSection">
+          <table className="fixed-size-table">
+            <thead>
+              <tr>
+                <th></th>
+                <th className="menuQuantity">Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menuData.map((menu) => (
+                <tr key={menu.id}>
+                  <td className="menuName">
+                    {capitalizeFirstLetters(menu.name)}
+                  </td>
+                  <td className="menuQuantity">
+                    <input
+                      type="number"
+                      name={menu.id}
+                      value={
+                        formData.menus.find((item) => item.menu_id === menu.id)
+                          ?.quantity || ""
+                      }
+                      onChange={handleMenuSelection}
+                    />
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td className="menuName">Total</td>
+                <td className="menuQuantity">
+                  <span>{total}</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {/* <div className="checkboxes"></div> */}
+        </div>
+
         <br />
         <button>{edit ? "EDIT" : "Create"}</button>
       </form>
-      {/* {edit && (
-        <DeleteMenu restaurantId={restaurantId.current} menuId={menuId} />
-      )} */}
     </div>
   );
 };
